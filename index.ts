c8eae416b433790b6e237afdebf051182cebecc0
WhiteNeofetch <63102175+WhiteNeofetch@users.noreply.github.com>
2022-01-17 05:38:11 +0300

    endpoint
    endpoint

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
+
+function handler(_req: Request): Response {
+  return new Response("Hello, World!");
+}
+
+console.log("Listening on http://localhost:8000");
+serve(handler);
